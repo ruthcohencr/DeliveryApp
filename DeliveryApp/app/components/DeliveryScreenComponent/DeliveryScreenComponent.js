820f6661c22f8e237522b6a05b0d57ad4ea9bfd9
Ruth Cohen <ruthcohencr@gmail.com>
2018-01-29 12:44:13 +0200

    Start working on google map API

--- DeliveryApp/app/components/DeliveryScreenComponent/DeliveryScreenComponent.js
+++ DeliveryApp/app/components/DeliveryScreenComponent/DeliveryScreenComponent.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, View, Image, Text, Button } from 'react-native';
+import { AppRegistry, StyleSheet, View, Image, Text, Button, Dimensions } from 'react-native';
 import { StackNavigator} from 'react-navigation';
 import ImageComponent from '../ImageComponent/ImageComponent';
 import { WEEK } from '../../mockdata/week.js';
+import MapView from 'react-native-maps';
 
+const {width, height} = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const SCREEN_WIDTH = width;
+const LATITUDE_DELTA = 0.0922;
+const LONGTITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class DeliveryScreenComponent extends Component {
 
@@ -12,7 +18,7 @@ export default class DeliveryScreenComponent extends Component {
       };
 
       openGoogleMap(){
-          
+          console.log ('open google map pressed');
       }
 
       //need to take this function out from hear
@@ -35,12 +41,7 @@ export default class DeliveryScreenComponent extends Component {
         const { navigate } = this.props.navigation;
         currentCustomer = this.props.navigation.state.params.customer;
         console.log('from deliveryScreen ' + currentCustomer.client['name']);
-     //   console.log('current customer name ' + this.props.navigation.state.params.customer.client['name'])
-
-      //  var currentCustomrer = this.props.navigation.state.params.customer;
-
-     //   console.log('currentCustomer = '+this.props.navigation.state.params);
-
+        console.log('latitude '+currentCustomer.address.geometry['coordinates'][0]);
         var timeFrom = new Date(currentCustomer['from']).getHours() + ':' + '00';
         var timeTo = new Date(currentCustomer['to']).getHours() + ':' + '00';
         var dayName = WEEK[ new Date(currentCustomer['to']).getDay()];
@@ -50,9 +51,18 @@ export default class DeliveryScreenComponent extends Component {
         return(
       
                 <View style={styles.container}>
-                    <View style ={styles.mapContainer}>
-                        <Text style={styles.goBack} onPress = {()=> navigate('MainScreen')}> Go Back </Text>
+                 <Text style={styles.goBack} onPress = {()=> navigate('MainScreen')}> Go Back </Text>
+                    <View style ={styles.mapContainer}>                        
+                        <MapView style = {styles.mapArea}
+                            region={{
+                                latitude:currentCustomer.address.geometry['coordinates'][0],
+                                longitude:currentCustomer.address.geometry['coordinates'][1],
+                                latitudeDelta: LATITUDE_DELTA,
+                                longitudeDelta:LONGTITUDE_DELTA,
+                            }}>
+                        </MapView>
                     </View>
+
                     <View style = {styles.detailsContainer}>
                              <View>
                                 <Text style={styles.id}>{currentCustomer['humanId']}</Text>  
@@ -62,29 +72,30 @@ export default class DeliveryScreenComponent extends Component {
                                         {' '+currentCustomer.address['city']} </Text>
                             </View>
                             <View style={styles.circle}>                 
-                                <ImageComponent imageUri='https://images.vexels.com/media/users/3/136563/isolated/preview/2acd4ce565e17c65e0ae15150248bf41-phone-call-stroke-icon-by-vexels.png'/>
+                                <Image style={styles.imageStyle} 
+                                 source={require('../../Images/iconCall2.png')} />
                             </View>                            
-                 </View>
-                 <View style={styles.ariveDayTime}>
-                 
-                    <View>
-                        <Text style={styles.allDate}> {dayName} </Text>
-                        <Text style={styles.allDate}> {displayDate} </Text>
-                        <Text style= {styles.estimatedArrival}> Estimated arrival:  </Text>
                     </View>
-                    <View>
+
+                    <View style={styles.ariveDayTime}>              
+                        <View>
+                            <Text style={styles.allDate}> {dayName} </Text>
+                            <Text style={styles.allDate}> {displayDate} </Text>
+                            <Text style= {styles.estimatedArrival}> Estimated arrival:  </Text>
+                        </View>
+                        <View>
                         <Text style={styles.arriveHour}>
                          {timeFrom}  -  {timeTo}  </Text>
+                        </View>                
                     </View>
-                    
-                </View>
-                <View style = {styles.buttonArea}>
-                    <Button style={styles.mapButton} 
-                        title = 'Open in google maps'
-                        onPress = {this.openGoogleMap}>           
-                    </Button>
-                </View>
-         </View>
+                    <View style={styles.mapButton} >
+                        <Button  color = 'rgb(66, 97, 144)'
+                            title = '  Open in google maps  '
+                            onPress = {this.openGoogleMap}>          
+                        </Button>
+                    </View> 
+                            
+            </View>
         );
     }
 }
@@ -95,19 +106,35 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         flex: 1,   
     },
+    mapContainer:{
+        flex: 1,
+      //  position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    mapArea:{  
+        position: 'absolute',
+        top:0,
+        bottom:0,
+        right:0,
+        left:0,
+    },
     goBack:{
         justifyContent: 'center',
         alignSelf:'center',
         fontSize: 25,
         paddingTop:20,
     },
-    mapContainer:{
-        flex: 1,
-    },
+  
     detailsContainer:{
         flex:0.4,
         flexDirection: 'row',
         backgroundColor: 'white',
+        position: 'relative',
         paddingLeft: 7,
         paddingTop: 15,
         paddingRight: 20,
@@ -137,9 +164,9 @@ const styles = StyleSheet.create({
       circle: {
         position:'absolute',
         right:10,
-        width: 50,
-        height: 50,
-        borderRadius: 50 / 2,
+        width: 55,
+        height: 55,
+        borderRadius: 55 / 2,
         backgroundColor: 'rgb(66, 97, 144)',
         justifyContent: 'center',
         alignSelf: 'center',
@@ -152,7 +179,8 @@ const styles = StyleSheet.create({
         paddingRight: 7,
       },
       buttonArea:{
-        flex:0.2,
+        flex:0.4,
+        backgroundColor:'white',
       },
       arriveHour: {
         backgroundColor:'rgb(244, 120, 139)',
@@ -170,10 +198,21 @@ const styles = StyleSheet.create({
         paddingLeft: 7,
       },
       mapButton:{
-        color: 'rgb(66, 97, 144)',
+       // color: 'rgb(66, 97, 144)',
+        zIndex:11,
+        backgroundColor:'white',
+        flex:0.3,
         alignItems: 'center',
         justifyContent: 'center',
-        borderRadius: 30,
+        borderRadius: 10,
+ 
+        shadowColor: '#000000',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+        shadowRadius: 10,
+        shadowOpacity: 0.25
       },
       estimatedArrival:{
         color: 'rgb(244, 120, 139)',
